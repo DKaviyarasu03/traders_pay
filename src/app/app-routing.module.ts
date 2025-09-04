ee3790d741db3221466ccf74378e181092d49827
Leenus Valantine A <leenusvalantine03@gmail.com>
2025-09-04 12:56:43 +0530

    Terms and conditions
    Terms and conditions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingPagesComponent } from './landing-pages/landing-pages.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TermsAndConditionsComponent } from './policies/terms-and-conditions/terms-and-conditions.component';
 
 const routes: Routes = [
    { path: '', component:LandingPagesComponent },
   { path: 'register', component:RegisterComponent },
   { path: 'login', component:LoginComponent },
   { path: 'dashboard', component:DashboardComponent },
+  { path: 'terms-and-conditions', component:TermsAndConditionsComponent},
 ];
 
 @NgModule({
